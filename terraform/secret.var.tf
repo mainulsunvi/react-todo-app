eaa1dc88d25a97e4af3d8f26cacbfaab561b90fd
Mainul I. Sunvi <mainulsunvi@gmail.com>
2023-04-15 02:16:43 +0600

    Update secret.var.tf

--- terraform/secret.var.tf
+++ terraform/secret.var.tf
@@ -2,8 +2,8 @@ variable "aws_sectrets" {
 	type = map
 	sensitive = true
 	default = {
-		"accessKey" = "AKIAQGDWYYR43RH2LOUX"
-		"secretKey" = "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"
+		"accessKey" = "Enter Access key"
+		"secretKey" = "Enter Secret Key"
 	}
 }
 
